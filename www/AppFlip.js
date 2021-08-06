2b439a44cb78ecd81e303395581474150b31e088
Chaitanya Mankala <chaithanya.mankala@gmail.com>
2021-08-06 18:50:19 +0530

    [update] use promises instead of callbacks

--- www/AppFlip.js
+++ www/AppFlip.js
@@ -1,25 +1,27 @@
-    var exec = require('cordova/exec');
-    
-    coolMethod = function (arg0, success, error) {
-        exec(success, error, 'AppFlip', 'coolMethod', [arg0]);
-    };
-    
-    var appflip = {
-        getInitialPushPayload: function (win, fail) {
-                   return cordova.exec(win, fail, "AppFlip", "getInitialPushPayload", []);
-                },
-        sendAuthCode: function(auth_data, win, fail) {
-        let code = auth_data.code ? auth_data.code : null;
-        let error_status = auth_data.status ? auth_data.status : null;
-        let error_message = auth_data.message ? auth_data.message : null;
-                return cordova.exec(win, fail, "AppFlip", "sendAuthCode", [code,error_status,error_message]);
-            }
-    }
-    
-    window.appflip = appflip;
-    
-    var AppFlip = {
-            appflip: appflip
-        };
-    
-      module.exports = AppFlip;    
+const pluginName = "AppFlip";
+
+var exec = function exec(method, params) {
+  return new Promise(function (resolve, reject) {
+    return cordova.exec(resolve, reject, pluginName, method, params);
+  });
+};
+
+var appflip = {
+  getInitialPushPayload: function () {
+    return exec("getInitialPushPayload", []);
+  },
+  sendAuthCode: function (auth_data) {
+    let code = auth_data.code ? auth_data.code : null;
+    let error_status = auth_data.status ? auth_data.status : null;
+    let error_message = auth_data.message ? auth_data.message : null;
+    return exec("sendAuthCode", [code, error_status, error_message]);
+  },
+};
+
+window.appflip = appflip;
+
+var AppFlip = {
+  appflip: appflip,
+};
+
+module.exports = AppFlip;
